f94ffbbddced652900e75971fc21836f922c30bc
Tiago Azevedo <17427@alunos.ipbeja.pt>
2022-08-05 11:45:04 +0100

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchQuizQuestions } from "./API";
 
-function App() {
+import QuestionCard from "./components/QuestionCard";
+
+import { QuestionState, Difficulty } from "./API";
+
+import { GlobalStyle, Wrapper } from "./App.styles";
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correct_answer: string;
+};
+
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const [nrQuestion, setNrQuestion] = useState('');
+
+  const numberQuestion = (e:React.ChangeEvent<HTMLInputElement>)=> {
+    const nrQuestions = e.target.value
+    setNrQuestion(nrQuestions)
+  }
+
+  const numberOfQuestions = parseInt(nrQuestion)
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      numberOfQuestions,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+
+      const correct = questions[number].correct_answer === answer;
+
+      if (correct) {
+        setScore((prev) => prev + 1);
+      }
+
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correct_answer: questions[number].correct_answer,
+      };
+
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === numberOfQuestions) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <div className="App">
+          <h1>Noobs Quiz</h1>
+          {gameOver ? <input
+              type="text"
+              className="numberOfQuestions"
+              placeholder="Enter number of questions"
+              value = { nrQuestion }
+              onChange={numberQuestion}
+            /> : null 
+          }
+            
+          {gameOver || userAnswers.length === numberOfQuestions ? (
+            <button className="start" onClick={startTrivia}>
+              Start
+            </button>
+          ) : null}
+          {!gameOver ? <p className="score">Score: {score}</p> : null}
+
+          {loading && <p>Loading Question...</p>}
+          {!loading && !gameOver && (
+            <QuestionCard
+              questionNumber={number + 1}
+              totalQuestions={numberOfQuestions}
+              question={questions[number].question}
+              answers={questions[number].answers}
+              userAnswer={userAnswers ? userAnswers[number] : undefined}
+              callback={checkAnswer}
+            />
+          )}
+          {!gameOver &&
+          !loading &&
+          userAnswers.length === number + 1 &&
+          number !== numberOfQuestions - 1 ? (
+            <button className="next" onClick={nextQuestion}>
+              Next Question
+            </button>
+          ) : null}
+        </div>
+      </Wrapper>
+    </>
   );
-}
+};
 
 export default App;
